a4773ad1f212cabf0eead07e36fe94d8c4e789e7
Shrapper <132104327+Shrapper@users.noreply.github.com>
2023-09-26 17:08:17 -0700

    Complete lesson 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, amount) {
+    let result = ''
+    for (let i = 0; i < amount; i++){
+        result += word
+    }
 
+    if (amount < 0) {
+        return 'ERROR'
+    }
+    return result
+
+    
 };
 
 // Do not edit below this line
